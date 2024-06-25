406408a3a04f7076f7474a3276e39925416a885a
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-06-25 10:30:39 -0500

    updated user model

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,4 +1,154 @@
 package com.technews.model;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+// This is a persistable object so the User class can map to a table
+@Entity
+// Ignores properties when serializing to JSON
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+// Specifies the table name that the class is mapping to
+@Table(name = "user")
+
+// We will need to make the class serializable, which converts the info to a bytestream to store in a database for the JVM
+public class User implements Serializable {
+    @Id
+    // The strategy argument says that the number will be generated automatically
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    // For the email annotation, ensure that it is unique
+    @Column(unique = true)
+    private String email;
+
+    private String username;
+    private String password;
+
+    // For the login variable, we add @Transient because we don't want the user always logged in
+    @Transient
+    boolean loggedIn;
+
+    // References for our other models
+    // Eager fetch types gather information immediately after creation. Fetch type Lazy only gathers info as needed
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Post> posts;
+
+    // Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Vote> votes;
+
+    // Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return isLoggedIn() == user.isLoggedIn() &&
+                Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getUsername(), user.getUsername()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getPosts(), user.getPosts()) &&
+                Objects.equals(getVotes(), user.getVotes()) &&
+                Objects.equals(getComments(), user.getComments());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getEmail(), getPassword(), isLoggedIn(), getPosts(), getVotes(), getComments());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", loggedIn=" + loggedIn +
+                ", posts=" + posts +
+                ", votes=" + votes +
+                ", comments=" + comments +
+                '}';
+    }
 }
